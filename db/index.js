21db4b9f8ba0512c3055fda48d824f0b18985667
Andriy Pylypets <a.pylypets95@gmail.com>
2019-10-29 19:31:51 +0100

    Added name of db

--- db/index.js
+++ db/index.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const uri = 'mongodb+srv://pylypanda:parol1233214@cluster0-csnvf.mongodb.net/test?retryWrites=true&w=majority'
+const uri = 'mongodb+srv://pylypanda:parol1233214@cluster0-csnvf.mongodb.net/test?retryWrites=true&w=majority/starchess'
 
 mongoose
     .connect(uri, { useNewUrlParser: true })
